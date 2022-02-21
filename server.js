9a7b50538aceeb055a5dabf24d25a8ec489e0503
Yaakov Yisrael Benjamin <yyisrael613@gmail.com>
2022-02-20 20:58:45 -0500

    added save notes functionality

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const fs = require("fs");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+});
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        const notes = JSON.parse(data);
+        res.json(notes)
+    })
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+
+    fs.readFile('./db/db.json', (err, data) => {
+        const notes = JSON.parse(data);
+        notes.push(newNote);
+
+        fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+            if(err) throw err;
+            res.send('Saved new Note');
+        })
+    })
+});
+
+app.listen(PORT, () => {
+  console.log(`Notetaker app listening at http://localhost:${PORT}`);
+});
